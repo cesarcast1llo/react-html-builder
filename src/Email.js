8aa807b8a74bd0b9f57d3a6df56554868927fab4
cesarcast1llo <cesarcast1llo@yahoo.com>
2022-03-18 12:07:06 -0700

    updates to js files set up, pulling in data next

--- src/Email.js
+++ src/Email.js
@@ -1,4 +1,4 @@
-import Body from './Components/Body';
+// import Body from './Components/Body';
 import Table from './Components/Table';
 import Tr from './Components/Tr';
 import Td from './Components/Td';
@@ -7,21 +7,20 @@ import Img from './Components/Img';
 
 function App() {
   return (
-    <Body style={{backgroundColor: "red"}}>
       <Table align="center" className="main" width="700" style={{width: "700px", backgroundColor: "#feffff"}}>
-        <Tr>
+         <Spacer height="70px"/>
+         <Tr>
           <Td align="center">
-            <Img src="https://d2axdqolvqmdvx.cloudfront.net/6f25960f-c392-4ae8-9149-1386313f1456/RedLogo.png"     alt="abbr" style={{maxWidth: "100px"}} />
+            <Img src="https://d2axdqolvqmdvx.cloudfront.net/6f25960f-c392-4ae8-9149-1386313f1456/RedLogo.png" alt="abbr" style={{maxWidth: "100px"}} />
           </Td>
         </Tr>
         <Spacer height="50px"/>
         <Tr>
           <Td align="left">
-            <Img src="https://d2axdqolvqmdvx.cloudfront.net/6f25960f-c392-4ae8-9149-1386313f1456/RedLogo.png"     alt="abbr" style={{maxWidth: "100px"}} />
+            <Img src="https://d2axdqolvqmdvx.cloudfront.net/6f25960f-c392-4ae8-9149-1386313f1456/RedLogo.png" alt="abbr" style={{maxWidth: "100px"}} />
           </Td>
         </Tr>
       </Table>
-    </Body>
   );
 }
 
