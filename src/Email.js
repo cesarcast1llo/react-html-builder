279934493dde224442a2fe04bef29d627de7c848
Cesar Castillo <cesarc@indeed.com>
2022-07-28 16:48:23 -0700

    updated npm packages for git erros

--- src/Email.js
+++ src/Email.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+// { useEffect, useState }
 import Table from './Components/ReusableElements/Table';
 import Header from './Components/Header';
 import Main from './Components/Main';
